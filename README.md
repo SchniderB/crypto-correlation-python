958bd325ff580db609d2e10be0df54562c1d0360
Boris <boris.schnider1994@gmail.com>
2020-11-29 20:00:00 +0100

    Improved the project structure, documentation, organization and reusability

--- README.md
+++ README.md
@@ -1,11 +1,71 @@
 # Cryptocurrency price correlations
 
 ## Aim of the project
-This project explains how to compute price correlations between different cryptocurrencies in Python.
+This project explains how to compute price correlations between different cryptocurrencies in Python based on price history 
+files of cryptocurrencies of interest.
 
 ## Dependencies
+### First set-up
+1. Download `virtualenv` if you do not alread have it
+```bash
+pip install virtualenv
+```
+2. Create virtual environment in your folder of interest
+```bash
+virtualenv crypto-correlations
+```
+3. Activate the virtual environment
+```bash
+source crypto-correlations/bin/activate
+```
+4. Install the libraries of interest in the virtual environment based on the `requirements.txt` file
+```bash
+pip install -r requirements.txt
+```
+
+### Re-activate virtual environment every time you need to extract data
+```bash
+source crypto-correlations/bin/activate
+```
+
 
 ## Instructions
+### Input files
+Each cryptocurrency history data file should be stored in the folder `crypto_history`. The price correlation script 
+`price_correlations.py` will extract the history data based on the following format:
+```
+price   volume  time    buy/sell        market/limit    miscellaneous   Since
+1.114431        18.07332223     1606777316.4328 b       l               1606777200.0
+1.121256        17.96331051     1606777381.7706 b       l               1606777200.0
+1.125000        998.40255591    1606777840.5439 b       m               1606777200.0
+...
+```
+The input file `crypto_history/LSKEUR_close_price.txt` was provided as an example. In case you want to use a different 
+format, make sure to modify the function `extract_mean_price_ph` in the script `price_correlation.py`.
+
+### Date and cryptocurrency list configuration
+The start date and end date timestampps should be defined in `config.txt` (please refer to the file for more detail 
+regarding the format). The graph first start and second start dates, which are simply two different time frames for 
+the graphs should be defined in this same configuration file. A large list of cryptocurrency symbols should be defined 
+in this same configuration file to compute large correlations. A short list of cryptocurrency symbols should be defined 
+in this same configuration file.
+
+### Compute the correlations
+Once the input price history files and the configuration file are ready, the following command will compute the 
+correlations:
+```bash
+python3 price_correlation.py
+```
 
 ## Results
+### Output example 1 - Clustermap
+![Cluster Heatmap 2019](output_plots/correlation_heatmap_2019.png)
 
+### Output example 2 - Large correlation matrix
+![Large Correlation Matrix](output_plots/correlation_2020.png)
+
+### Output example 3 - Small correlation matrix
+![Small Correlation Matrix](output_plots/highest_correlation_2020.png)
+
+### Warning
+There seems to be an issue with the significance symbol association on the plots.
--- price_correlation.py
+++ price_correlation.py
@@ -1,13 +1,30 @@
-#!/home/boris/Documents/Trading_bot/.virtualenv/krakenex/bin/python
 # -*- coding: utf-8 -*-
-
 """
-This script computes a Pearson correlation between the BTC price and the price of
-any other cryptocurrency.
+Cryptocurrency Price Correlation Analysis
 
-Created on Sat Nov 28 21:28:53 2020
+This script computes Pearson correlations between the price histories of different cryptocurrencies.
+The price history data is read from files in the `crypto_history` folder. The script generates
+correlation matrices, heatmaps, and scatterplots to visualize the relationships between cryptocurrencies.
 
-@author: boris
+Configuration values (e.g., time range, cryptocurrency lists) are loaded from a `config.txt` file.
+The file format is as follows:
+    start_time=1380585600
+    end_time=1597449600
+    first_graph_start=1546300800
+    second_graph_start=1577836800
+    large_list=ADA,XXRP,XXBT,XMLN,DASH,XETH,LINK,XLTC
+    short_list=XXRP,XXBT,XETH,XLTC
+
+Features:
+- Extract hourly price averages or returns for cryptocurrencies.
+- Compute and visualize correlation matrices and heatmaps.
+- Analyze specified cryptocurrency subsets.
+
+Dependencies:
+- numpy, pandas, matplotlib, seaborn, scipy
+
+Created on: Sat Nov 28 21:28:53 2020
+Author: Boris
 """
 
 import os
@@ -17,11 +34,41 @@ import seaborn as sns
 from scipy import stats
 import pandas as pd
 
+# Configuration variables defined in config.txt
+CONFIG_FILE = 'config.txt'
+config = {}
+with open(CONFIG_FILE, 'r') as f:
+    for line in f:
+        key, value = line.strip().split('=')
+        config[key] = value
+
+START_TIME = int(config['start_time'])
+END_TIME = int(config['end_time'])
+FIRST_GRAPH_SLICE = int(config['first_graph_start'])
+SECOND_GRAPH_SLICE = int(config['second_graph_start'])
+LARGE_LIST = config['large_list'].split(',')
+SHORT_LIST = config['short_list'].split(',')
+
+
 def extract_mean_price_ph(file, all_hours):
     """
-    Function that extracts the mean price per hour of a given cryptocurrency.
+    Extracts the mean hourly price of a given cryptocurrency.
+
+    This function processes a cryptocurrency price history file to compute
+    either the mean price for each hour within a specified time range.
+
+    :param file: String path to the cryptocurrency price file.
+    :param all_hours: List of timestamps representing hourly intervals
+                        over the desired time range.
+    :return: tuple: Two lists -
+            crypto_time (list): Timestamps corresponding to the hours.
+            crypto_price (list): Hourly price average (or "NA" if no trades occurred).
+
+    Note:
+        - The input file must be tab-delimited with the following columns:
+          close price, volume, timestamp (in seconds).
+        - Trades occurring within a given hour contribute to the hourly price average.
     """
-    os.chdir("/home/boris/Documents/Trading_bot/benchmark_data/correlation")
     crypto_price = []
     crypto_time = []
     j = 0
@@ -66,12 +113,39 @@ def extract_mean_price_ph(file, all_hours):
     return crypto_time, crypto_price
 
 def from_pair_to_crypto(pair_name):
+    """
+    Converts a cryptocurrency pair name into a standalone cryptocurrency symbol.
+
+    This function removes the EUR suffix (either "EUR" or "ZEUR") from
+    cryptocurrency pair names used in Kraken trading data.
+
+    :param pair_name: Pair name string, e.g., "XXBTZEUR" or "ADAEUR".
+    :return: String, cryptocurrency symbol, e.g., "XXBT" or "ADA".
+
+    Note: Special handling is applied for pairs like "XTZEUR".
+    """
     if pair_name[-4:] == "ZEUR" and pair_name != "XTZEUR":
         return pair_name.rstrip("ZEUR")
     elif pair_name[-3:] == "EUR":
         return pair_name.rstrip("EUR")
 
-def corrfunc(x, y, **kws):
+def corrfunc(x, y, **kws):"""
+    Computes the Pearson correlation coefficient and annotates it on a plot.
+
+    This function calculates the correlation coefficient (r) and its 
+    significance level (p-value). If significant, an asterisk notation 
+    (*, **, ***) is added to indicate the level of significance.
+
+    :param x: First data series.
+    :param y: Second data series.
+
+    Annotation:
+        - r: Correlation coefficient rounded to 2 decimal places.
+        - Significance: 
+            *   p <= 0.05
+            **  p <= 0.01
+            *** p <= 0.001
+    """
     nas = np.logical_or(np.isnan(x), np.isnan(y))  # find coordinates of NaNs in x and y
     r, p = stats.pearsonr(x[~nas], y[~nas])  # remove coordinate if either in x or in y
     p_stars = ''
@@ -85,11 +159,33 @@ def corrfunc(x, y, **kws):
     ax.annotate("r = {:.2f} {}".format(r, p_stars), xy=(0.05, 0.9), xycoords=ax.transAxes)
 
 def annotate_colname(x, **kws):
-  ax = plt.gca()
-  ax.annotate(x.name, xy=(0.05, 0.9), xycoords=ax.transAxes,
+    """
+    Annotates the column name on the diagonal of a seaborn PairGrid.
+
+    :param x: Data for the diagonal plot.
+    """
+    ax = plt.gca()
+    ax.annotate(x.name, xy=(0.05, 0.9), xycoords=ax.transAxes,
               fontweight='bold')
 
 def cor_matrix(df):
+    """
+    Creates a comprehensive correlation matrix visualization for a given DataFrame.
+
+    This function generates a seaborn PairGrid with the following features:
+    - Upper triangle: Scatterplots with regression lines.
+    - Diagonal: Histograms with column name annotations.
+    - Lower triangle: Correlation coefficients with significance levels
+                      and kernel density plots.
+
+    :param df: DataFrame containing cryptocurrency price data.
+    :return: The configured PairGrid for further customization or saving.
+
+    Note:
+        - NaN values are ignored when computing correlations.
+        - This visualization is useful for detecting patterns and relationships
+          between multiple variables.
+    """
     sns.set(style='white')
     g = sns.PairGrid(df, dropna=True)
     # Use normal regplot as `lowess=True` doesn't provide CIs.
@@ -105,121 +201,126 @@ def cor_matrix(df):
 
     return g
 
-# Define the main variables
-first_hour = 1380585600  # 1st of October 2013 at 00:00:00 is the first hour of the count
-last_hour = 1597449600  # 15th of August 2020 at 00:00:00 is the last hour of the count
-all_hours = list(range(1380585600, 1597449600, 3600))  # All rounded hours from start time to end time
-crypto_price_ph = dict()
 
-# Process all the cryptocurrency records
-list_files = [file for file in sorted(os.listdir("..")) if "close_price" in file]
+if __name__ == "__main__":
+    # Define the main variables
+    all_hours = list(range(START_TIME, END_TIME, 3600))  # All rounded hours from start time to end time
+    crypto_price_ph = dict()
 
-cryptocurrencies = []
-for crypto_file in list_files:
-    crypto = crypto_file.split("_")[0]
-    cryptocurrencies.append(crypto)
-    crypto_price_ph[crypto] = [[], []]
-    crypto_price_ph[crypto][0], crypto_price_ph[crypto][1] = extract_mean_price_ph("../{}".format(crypto_file), all_hours)
-print("Extraction DONE")
+    # Process all the cryptocurrency records
+    # List all files in the "crypto_history" directory that contain "close_price" in their filenames
+    list_files = [file for file in sorted(os.listdir("crypto_history")) if "close_price" in file]
 
-# Generate dictionaries per date
-prices_2019 = dict()
-prices_2020 = dict()
-prices_20192020 = dict()
-for crypto in cryptocurrencies:
-    i = 0
-    j = 0
-    i = crypto_price_ph[crypto][0].index(1546300800)
-    name = from_pair_to_crypto(crypto)
-    prices_20192020[name] = crypto_price_ph[crypto][1][i:]  # 2019-now
-    j = crypto_price_ph[crypto][0].index(1577836800)
-    prices_2020[name] = crypto_price_ph[crypto][1][j:]  # 2020-now
-    if [val for val in crypto_price_ph[crypto][1][i:j] if val != "NA"]:  # 2019-2020, take only crypto if any data in 2019
-        prices_2019[name] = crypto_price_ph[crypto][1][i:j]
-print("Dictionary split per date")
+    # Initialize a list to store cryptocurrency symbols and a dictionary for hourly prices
+    cryptocurrencies = []
+    for crypto_file in list_files:
+        # Extract the cryptocurrency symbol from the file name (everything before the first underscore)
+        crypto = crypto_file.split("_")[0]
+        cryptocurrencies.append(crypto)  # Add the symbol to the list
 
-# Convert to dataframe
-df_2019 = pd.DataFrame(prices_2019)
-key_list = [i for i in prices_2019.keys()]
-df_2019[key_list] = df_2019[key_list].replace({'NA':np.nan})
-df_2019_sub = df_2019[["ADA", "XXRP", "XXBT", "XMLN", "DASH", "XETH", "LINK", "XLTC"]]
-df_2019_sub2 = df_2019[["XXRP", "XXBT", "XETH", "XLTC"]]
-# corrfunc(df_2019[key_list[0]], df_2019[key_list[1]])
+        # Initialize a placeholder for storing time and price data for the cryptocurrency
+        crypto_price_ph[crypto] = [[], []]
 
-df_2020 = pd.DataFrame(prices_2020)
-key_list = [i for i in prices_2020.keys()]
-df_2020[key_list] = df_2020[key_list].replace({'NA':np.nan})
-df_2020_sub = df_2020[["ADA", "XXRP", "XXBT", "XMLN", "DASH", "XETH", "LINK", "XLTC"]]
-df_2020_sub2 = df_2020[["XXRP", "XXBT", "XETH", "XLTC"]]
+        # Extract hourly mean price and corresponding timestamps for the cryptocurrency
+        crypto_price_ph[crypto][0], crypto_price_ph[crypto][1] = extract_mean_price_ph("crypto_history/{}".format(crypto_file), all_hours)
+    print("Extraction DONE")
 
-df_20192020 = pd.DataFrame(prices_20192020)
-key_list = [i for i in prices_20192020.keys()]
-df_20192020[key_list] = df_20192020[key_list].replace({'NA':np.nan})
-df_20192020_sub = df_20192020[["ADA", "XXRP", "XXBT", "XMLN", "DASH", "XETH", "LINK", "XLTC"]]
-df_20192020_sub2 = df_20192020[["XXRP", "XXBT", "XETH", "XLTC"]]
-print("Conversion to dataframe DONE")
+    # Generate dictionaries of prices segmented by year
+    prices_2019 = dict()      # Prices for the year 2019
+    prices_2020 = dict()      # Prices for the year 2020
+    prices_20192020 = dict()  # Prices from 2019 through 2020
 
-# Plot the data
-full_corr_matrix_19 = df_2019.corr()
-full_corr_matrix_20 = df_2020.corr()
-full_corr_matrix_1920 = df_20192020.corr()
-print(full_corr_matrix_19)
-exit()
+    for crypto in cryptocurrencies:
+        # Initialize indices for slicing the time series data
+        i = 0
+        j = 0
 
-g = sns.clustermap(full_corr_matrix_19, vmin=-1, vmax=1, center=0, linewidths=.5, cbar_kws={"shrink": .5}, xticklabels=True, yticklabels=True, cmap="RdBu_r")
-g.ax_row_dendrogram.remove()
-plt.savefig("correlation_heatmap_2019.png", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
-print(full_corr_matrix_19)
+        # Find the index corresponding to the start of 2019 (e.g. timestamp: 1546300800)
+        i = crypto_price_ph[crypto][0].index(FIRST_GRAPH_SLICE)
 
-g = sns.clustermap(full_corr_matrix_20, vmin=-1, vmax=1, center=0, linewidths=.5, cbar_kws={"shrink": .5}, xticklabels=True, yticklabels=True, cmap="RdBu_r")
-g.ax_row_dendrogram.remove()
-plt.savefig("correlation_heatmap_2020.png", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+        # Convert pair symbols (e.g., BTCZEUR) to cryptocurrency names (e.g., BTC)
+        name = from_pair_to_crypto(crypto)
 
-g = sns.clustermap(full_corr_matrix_1920, vmin=-1, vmax=1, center=0, linewidths=.5, cbar_kws={"shrink": .5}, xticklabels=True, yticklabels=True, cmap="RdBu_r")
-g.ax_row_dendrogram.remove()
-plt.savefig("correlation_heatmap_20192020.png", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+        # Store all prices from 2019 onward in the combined dictionary
+        prices_20192020[name] = crypto_price_ph[crypto][1][i:]  # 2019-now
 
-cor_matrix(df_2019_sub)
-plt.savefig("correlation_2019.png", bbox_inches="tight", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+        # Find the index corresponding to the start of 2020 (e.g. timestamp: 1577836800)
+        j = crypto_price_ph[crypto][0].index(SECOND_GRAPH_SLICE)
 
-cor_matrix(df_2020_sub)
-plt.savefig("correlation_2020.png", bbox_inches="tight", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+        # Store prices from 2020 onward in the 2020 dictionary
+        prices_2020[name] = crypto_price_ph[crypto][1][j:]  # 2020-now
 
-cor_matrix(df_20192020_sub)
-plt.savefig("correlation_2019-2020.png", bbox_inches="tight", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+        # Only include cryptocurrencies with valid data for 2019 in the 2019 dictionary
+        if [val for val in crypto_price_ph[crypto][1][i:j] if val != "NA"]:  # 2019-2020, take only crypto if any data in 2019
+            prices_2019[name] = crypto_price_ph[crypto][1][i:j]
+    print("Dictionary split per date")
 
-cor_matrix(df_2019_sub2)
-plt.savefig("highest_correlation_2019.png", bbox_inches="tight", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+    # Convert price dictionaries to DataFrames for analysis
+    df_2019 = pd.DataFrame(prices_2019)  # DataFrame for 2019 prices
+    key_list = [i for i in prices_2019.keys()]  # List of cryptocurrency names in the 2019 dataset
+    df_2019[key_list] = df_2019[key_list].replace({'NA':np.nan})  # Replace "NA" with NaN for easier numerical processing
+    df_2019_sub = df_2019[LARGE_LIST]  # Subset for cryptocurrencies in the large list
+    df_2019_sub2 = df_2019[SHORT_LIST]  # Subset for cryptocurrencies in the short list
+    # corrfunc(df_2019[key_list[0]], df_2019[key_list[1]])
 
-cor_matrix(df_2020_sub2)
-plt.savefig("highest_correlation_2020.png", bbox_inches="tight", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+    df_2020 = pd.DataFrame(prices_2020)
+    key_list = [i for i in prices_2020.keys()]
+    df_2020[key_list] = df_2020[key_list].replace({'NA':np.nan})
+    df_2020_sub = df_2020[LARGE_LIST]
+    df_2020_sub2 = df_2020[SHORT_LIST]
 
-cor_matrix(df_20192020_sub2)
-plt.savefig("highest_correlation_2019-2020.png", bbox_inches="tight", dpi=300)
-plt.clf()#clears the figure to be able to write the next plot
-plt.cla()
-plt.close()
+    df_20192020 = pd.DataFrame(prices_20192020)
+    key_list = [i for i in prices_20192020.keys()]
+    df_20192020[key_list] = df_20192020[key_list].replace({'NA':np.nan})
+    df_20192020_sub = df_20192020[LARGE_LIST]
+    df_20192020_sub2 = df_20192020[SHORT_LIST]
+    print("Conversion to dataframe DONE")
+
+    # Format data for clustermaps
+    # Compute correlation matrices for each dataset
+    full_corr_matrix_19 = df_2019.corr()
+    full_corr_matrix_20 = df_2020.corr()
+    full_corr_matrix_1920 = df_20192020.corr()
+
+
+    ##### ----- Plot the data ----- #####
+
+    ##### ----- Start: Clustermaps ----- #####
+    # Clustermap data and corresponding filenames
+    clustermap_data = [
+        (full_corr_matrix_19, "output_plots/correlation_heatmap_2019.png"),
+        (full_corr_matrix_20, "output_plots/correlation_heatmap_2020.png"),
+        (full_corr_matrix_1920, "output_plots/correlation_heatmap_20192020.png"),
+    ]
+
+    # Generate clustermap plots
+    for matrix, filename in clustermap_data:
+        g = sns.clustermap(matrix, vmin=-1, vmax=1, center=0, linewidths=.5,
+                           cbar_kws={"shrink": .5}, xticklabels=True,
+                           yticklabels=True, cmap="RdBu_r")
+        g.ax_row_dendrogram.remove()
+        plt.savefig(filename, dpi=300)
+        plt.clf()  # Clear the figure
+        plt.cla()
+        plt.close()
+    ##### ----- End: Clustermaps ----- #####
+
+    ##### ----- Start: Correlation plots ----- #####
+    # Correlation matrix data and corresponding filenames
+    correlation_data = [
+        (df_2019_sub, "output_plots/correlation_2019.png"),
+        (df_2020_sub, "output_plots/correlation_2020.png"),
+        (df_20192020_sub, "output_plots/correlation_2019-2020.png"),
+        (df_2019_sub2, "output_plots/highest_correlation_2019.png"),
+        (df_2020_sub2, "output_plots/highest_correlation_2020.png"),
+        (df_20192020_sub2, "output_plots/highest_correlation_2019-2020.png"),
+    ]
+
+    # Generate correlation matrix plots
+    for df, filename in correlation_data:
+        cor_matrix(df)
+        plt.savefig(filename, bbox_inches="tight", dpi=300)
+        plt.clf()  # Clear the figure
+        plt.cla()
+        plt.close()
+    ##### ----- End: Correlation plots ----- #####
